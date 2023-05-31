ab617ae19ffc68954d4f6199888a52f424b77383
Srushhhti <ssrushti108@gmail.com>
2023-05-31 12:49:08 +0530

    added git blame command, filePath and working on match the email

--- extension.js
+++ extension.js
@@ -1,25 +1,47 @@
-// The module 'vscode' contains the VS Code extensibility API
+//we are using javascript for this that is why we have to use the require function
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
+//import exec to execute shell command
+const { exec } = require('child_process');
+const { stdout } = require('process');
 
-// This method is called when your extension is activated
+
 // Your extension is activated the very first time the command is executed
 
 /**
  * @param {vscode.ExtensionContext} context
  */
+
+// This method is called when your extension is activated
 function activate(context) {
+	console.log('Your extension "vs-code-extension-for-git-blame" is now active!');
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "vs-code-extension-for-git-blame" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
+	// Now provide the implementation of the command with  registerCommand it uses vscode.commands.registerCommand(commandId, callback);
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('vs-code-extension-for-git-blame.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
+	let disposable = vscode.commands.registerCommand('vs-code-extension-for-git-blame.showGitBlame', function () {
+		// The code placed here will be executes
+		//vscode.window -> shows that whether the window of vscode is running on not
+		//active Text Editor -> shows whether the editor is active on vs code or is not working on the same 
+		const editor = vscode.window.activeTextEditor;
+		//if(editor)=true when editor does soem work on the text 
+		if(editor){
+			//the filePath will have the information of teh file system oath of the doc on wuth editi=or is editing 
+			const filePath = editor.document.uri.fsPath;
+			//exec(command which have to be executed, callback func)
+			exec(`git blame --line-porcelain ${filePath}`, (error,stdout) => {
+				if(error){
+					console.error("Error occured during the command!");
+				}
+				//first then it splits the git blame command that is retrieved on the pc and then splits it at enter 
+				const gitBlameData = stdout.split('\n');
 
+				//then we execute this function to retrieve the editor's email id by checking each line that we got of gitBlameData
+				gitBlameData.forEach((line) => {
+					// we find a match for the editor's email
+				});
+
+			});
+		}
 		// Display a message box to the user
 		vscode.window.showInformationMessage('Hello World from VS code extension for git blame!');
 	});
